a1a669e1675ff171850b230505b9f2c7ede40a93
Dostin89 <mazadosti@gmail.com>
2024-10-25 21:53:46 -0500

    taller3

--- Proyecto3/src/Ejercicio6.java
+++ Proyecto3/src/Ejercicio6.java
@@ -8,5 +8,16 @@
  * @author IK
  */
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        double v1, v2, v3, v4, v5;
+        boolean resultado;
+                
+        v1 = Math.sqrt(81+9);
+        v2 = 3;
+        v3 = 9;
+        v4 = 10;
+        v5 = 1;
+        resultado = ((v1/v2) == v3) && (v4 > v5);
+        System.out.println("resultado = " + resultado);  
+    }
 }
--- Proyecto3/src/Ejercicio7.java
+++ Proyecto3/src/Ejercicio7.java
@@ -8,5 +8,24 @@
  * @author IK
  */
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        double v1, v2, v3, v4, v5, v6, v7, v8;
+        boolean resultado;
+        
+        v1 = Math.sqrt(81)+9;
+        v2 = 3;
+        v3 = 9;
+        v4 = 10;
+        v5 = 1;
+        v6 = 100;
+        v7 = 25;
+        v8 = Math.sqrt(100);
+        //se cambio el simbolo de operacion + por el simbolo logico >= para poder comparar.
+        resultado = ((v1/v2) == v3) || (v4 > v5) && ((v6/v7)>=v8);
+        System.out.println("resultado = " + resultado);
+               
+                
+        
+    }
     
 }
