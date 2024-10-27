e5b89d9fa1eae35632ee598b8e98332ae31e4f3e
Dostin89 <mazadosti@gmail.com>
2024-10-26 20:59:05 -0500

    taller3

--- Proyecto3/src/Ejercicio7.java
+++ Proyecto3/src/Ejercicio7.java
@@ -20,7 +20,7 @@ public class Ejercicio7 {
         v6 = 100;
         v7 = 25;
         v8 = Math.sqrt(100);
-        //se cambio el simbolo de operacion + por el simbolo logico >= para poder comparar.
+        
         resultado = ((v1/v2) == v3) || (v4 > v5) && ((v6/v7)>=v8);
         System.out.println("resultado = " + resultado);
                
